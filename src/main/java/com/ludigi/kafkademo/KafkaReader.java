3f63eccbc79daadbcfa1f27c2c6f5a96dce563ac
Sławek Ludwiczak <slawek.ludwiczak@gmail.com>
2025-11-13 22:27:11 +0100

    init

---
+++ src/main/java/com/ludigi/kafkademo/KafkaReader.java
@@ -0,0 +1,11 @@
+package com.ludigi.kafkademo;
+
+import org.springframework.kafka.annotation.KafkaListener;
+
+public class KafkaReader {
+
+    @KafkaListener
+    void readMessage() {
+
+    }
+}
